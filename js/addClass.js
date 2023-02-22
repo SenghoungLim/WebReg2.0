08097266c902276bc56450d33b15f9693b6f59b1
Hassan <hhalnajj@uci.edu>
2023-02-21 17:39:48 -0800

    fixed styling

--- js/addClass.js
+++ js/addClass.js
@@ -50,30 +50,30 @@ function Lab(status, code, type, units, instructor, schedule, location, enrollme
 
 const courses = [
 
-new Course("Intro to Programming", "Open", "COMP101", "Lecture", 4, "Dr. Johnson", "MWF 10:00AM-11:00AM", "Building 1 Room 101", 150, 300, 10, 20, "Computer Science", "Fall", [
-  new Discussion("Open", "COMP101A", "Discussion", 1, "John Smith", "M 1:00PM-2:00PM", "Building 1 Room 201", 25, 30, 5, 10),
-  new Discussion("Open", "COMP101B", "Discussion", 1, "Sarah Lee", "T 1:00PM-2:00PM", "Building 1 Room 202", 25, 25, 5, 10),
+new Course("Intro to Programming", "Open", "COMP101", "Lecture", 4, "Dr. Johnson", "MWF 10-11:00 AM", "Building 1 Room 101", 150, 300, 10, 20, "Computer Science", "Fall", [
+  new Discussion("Open", "COMP101A", "Discussion", 1, "John Smith", "M 1-2:00 PM", "Building 1 Room 201", 25, 30, 5, 10),
+  new Discussion("Open", "COMP101B", "Discussion", 1, "Sarah Lee", "T 1-2:00 PM", "Building 1 Room 202", 25, 25, 5, 10),
 ], [
-  new Lab("Open", "COMP101L1", "Lab", 1, "Alex Brown", "F 1:00PM-3:00PM", "Building 2 Room 301", 25, 30, 5, 10),
-  new Lab("Open", "COMP101L2", "Lab", 1, "Emily Kim", "W 1:00PM-3:00PM", "Building 2 Room 302", 25, 30, 5, 10),
+  new Lab("Open", "COMP101L1", "Lab", 1, "Alex Brown", "F 1-3:00 PM", "Building 2 Room 301", 25, 30, 5, 10),
+  new Lab("Open", "COMP101L2", "Lab", 1, "Emily Kim", "W 1-3:00 PM", "Building 2 Room 302", 25, 30, 5, 10),
 ]),
 
 
-new Course("Calculus I", "Open", "MATH101", "Lecture", 4, "Dr. Lee", "MWF 9:00AM-10:00AM", "Building 1 Room 102", 100, 150, 5, 10, "Mathematics", "Fall", [
-  new Discussion("Open", "MATH101A", "Discussion", 1, "Jessica Chen", "W 10:00AM-11:00AM", "Building 1 Room 203", 25, 30, 5, 10),
-  new Discussion("Open", "MATH101B", "Discussion", 1, "David Kim", "F 10:00AM-11:00AM", "Building 1 Room 204", 25, 30, 5, 10),
+new Course("Calculus I", "Open", "MATH101", "Lecture", 4, "Dr. Lee", "MWF 9-10:00 AM", "Building 1 Room 102", 100, 150, 5, 10, "Mathematics", "Fall", [
+  new Discussion("Open", "MATH101A", "Discussion", 1, "Jessica Chen", "W 10-11:00 AM", "Building 1 Room 203", 25, 30, 5, 10),
+  new Discussion("Open", "MATH101B", "Discussion", 1, "David Kim", "F 10-11:00 AM", "Building 1 Room 204", 25, 30, 5, 10),
 ], [
-  new Lab("Open", "MATH101L1", "Lab", 1, "Chris Lee", "Th 1:00PM-3:00PM", "Building 2 Room 303", 25, 30, 5, 10),
-  new Lab("Open", "MATH101L2", "Lab", 1, "Jennifer Park", "T 1:00PM-3:00PM", "Building 2 Room 304", 25, 30, 5, 10),
+  new Lab("Open", "MATH101L1", "Lab", 1, "Chris Lee", "Th 1-3:00 PM", "Building 2 Room 303", 25, 30, 5, 10),
+  new Lab("Open", "MATH101L2", "Lab", 1, "Jennifer Park", "T 1-3:00 PM", "Building 2 Room 304", 25, 30, 5, 10),
 ]),
 
 
-new Course("Calculus II", "Open", "MATH101", "Lecture", 4, "Dr. Lee", "MWF 9:00AM-10:00AM", "Building 1 Room 102", 100, 150, 5, 10, "Mathematics", "Fall", [
-  new Discussion("Open", "MATH101A", "Discussion", 1, "Jessica Chen", "W 10:00AM-11:00AM", "Building 1 Room 203", 25, 30, 5, 10),
-  new Discussion("Open", "MATH101B", "Discussion", 1, "David Kim", "F 10:00AM-11:00AM", "Building 1 Room 204", 25, 30, 5, 10),
+new Course("Calculus II", "Open", "MATH101", "Lecture", 4, "Dr. Lee", "MWF 9-10:00 AM", "Building 1 Room 102", 100, 150, 5, 10, "Mathematics", "Fall", [
+  new Discussion("Open", "MATH101A", "Discussion", 1, "Jessica Chen", "W 10-11:00 AM", "Building 1 Room 203", 25, 30, 5, 10),
+  new Discussion("Open", "MATH101B", "Discussion", 1, "David Kim", "F 10-11:00 AM", "Building 1 Room 204", 25, 30, 5, 10),
 ], [
-  new Lab("Open", "MATH101L1", "Lab", 1, "Chris Lee", "Th 1:00PM-3:00PM", "Building 2 Room 303", 25, 30, 5, 10),
-  new Lab("Open", "MATH101L2", "Lab", 1, "Jennifer Park", "T 1:00PM-3:00PM", "Building 2 Room 304", 25, 30, 5, 10),
+  new Lab("Open", "MATH101L1", "Lab", 1, "Chris Lee", "Th 1-3:00 PM", "Building 2 Room 303", 25, 30, 5, 10),
+  new Lab("Open", "MATH101L2", "Lab", 1, "Jennifer Park", "T 1-3:00 PM", "Building 2 Room 304", 25, 30, 5, 10),
 ]),
 
 

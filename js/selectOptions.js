a0802b681e694ed4f346325036b44d0a382377e2
Hassan <hhalnajj@uci.edu>
2023-03-07 02:57:54 -0800

    Fixed filter implementation

---
+++ js/selectOptions.js
@@ -0,0 +1,33 @@
+function getAllDepartments() {
+    l = []
+    for (let i = 0; i< courses.length; i++) {
+        l.push(courses[i].department);
+    }
+    return l
+}
+
+function getAllInstructors() {
+    l = []
+    for (let i = 0; i< courses.length; i++) {
+        l.push(courses[i].instructor);
+    }
+    return l
+}
+
+
+const departmentSelect = document.getElementById('department');
+const instructorSelect = document.getElementById('instructor');
+
+for (department of getAllDepartments()) {
+    const opt = document.createElement('option');
+    opt.value = department.replace(/ +/g, '-').toLowerCase();
+    opt.innerText = department;
+    departmentSelect.appendChild(opt);
+}
+
+for (instructor of getAllInstructors()) {
+    const opt = document.createElement('option');
+    opt.value = instructor.replace(/ +/g, '-').toLowerCase();
+    opt.innerText = instructor;
+    instructorSelect.appendChild(opt);
+}
